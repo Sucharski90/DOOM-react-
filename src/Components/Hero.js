54b71eabbe6a9d43a75ae4f077170796d20c6588
Matt <MVSucharski@gmail.com>
2018-06-22 03:45:27 -0400

    added side bar

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,9 +5,16 @@ class Hero extends Component {
   render() {
     return (
       <div className="Hero">
-      <a href="#.Ogdoom"><h1>Doom 1993</h1></a>
+      <a href="#Ogdoom"><h1>Doom 1993</h1></a>
         <h1>Doom 2016</h1>
-        
+        <div id="NavigationContainer">
+        <div class="sidenav">
+  <a href="#about">About</a>
+  <a href="#services">Services</a>
+  <a href="#clients">Clients</a>
+  <a href="#contact">Contact</a>
+</div>
+  </div>
       </div>
     );
   }
